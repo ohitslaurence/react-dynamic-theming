82bec90cfc072e9e0c056d8d4cc4031817ace32b
ljdavies <laurence.davies@libryo.com>
2020-04-18 20:09:33 +0200

    Adds and configures tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// postcss.config.js
+const tailwindcss = require('tailwindcss');
+
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    tailwindcss('./tailwind.js'),
+    require('autoprefixer'),
+  ],
+};
